f927bd2e2389a720420c6372d48a38fc406d3a4d
Sebastiandankner <225351238+Sebby34@users.noreply.github.com>
2026-04-03 04:50:27 -0400

    Update config with SECRET_KEY

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     DEBUG = True 
     CACHE_TYPE = "SimpleCache"
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
 class TestingConfig: 
     pass 
